17c6b85d7f54502c86d2900eec1d9f22c2370b54
Rian <rianbarroso596@gmail.com>
2023-01-18 17:15:23 -0300

    inserindo objetos

--- routes.js
+++ routes.js
@@ -1,8 +1,33 @@
+require('dotenv').config();
 const express = require('express');
 const routes = express.Router();
+const dbName = "CRUDatabase";
+const url = process.env.CONNECTSTRING
+const colecao = "users";
+const mongodb = require('mongodb').MongoClient;
+
+//start database
+mongodb.connect(url, (err, client) => {
+    if (err) throw err;
+    const dbo = client.db(dbName);
 
 
-
-
+    //Get Objects
+    routes.get('/', (req, res) => {
+        res.send('hello world!');
+    })
+    
+    //Insert Objects
+    routes.post('/:nome/:sobrenome', (req, res) => {
+        nome = req.params.nome
+        sobrenome = req.params.sobrenome
+        
+        dbo.collection(colecao).insertOne({ nome, sobrenome }, (err, result) => {
+            if (err) throw err;
+            res.send('Objetos inseridos com sucesso!')
+            client.close();
+        })
+    })
+})
 
 module.exports = routes
--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const routes = require('./routes');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-//mongoose connecting
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('database connected')
-        app.emit('pronto!')
-    }).catch(e => console.log(e))
 
 //crud routes
 app.use(routes);
