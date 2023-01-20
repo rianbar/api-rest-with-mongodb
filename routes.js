abdc4e1918472985212e0c0869ff6c562c9b1b6d
Rian <rianbarroso596@gmail.com>
2023-01-20 13:12:00 -0300

    basic queries update

--- routes.js
+++ routes.js
@@ -23,11 +23,69 @@ routes.post('/', async (req, res) => {
 
 //SELECT
 routes.get('/', async (req, res) => {
-    await Person.find({}, (err, docs) => {
-        if (err) throw err;
+    try {
+        const docs = await Person.find({});
         res.status(200).json(docs);
-    });
+    } catch(error) {
+        res.status(404).json({
+            message: "Ocorreu um erro na sua requisição!"
+        })
+    }
 
+})
+
+//FIND ONE
+routes.get('/:id', async (req, res) => {
+    try {
+        const doc = await Person.findOne({ _id: req.params.id });
+        res.status(200).json(doc);
+    } catch(error) {
+        res.status(404).json({
+            message: "Ocorreu um erro na sua requisição!"
+        })
+    }
+})
+
+//UPDADE
+routes.patch('/update/:id', async (req, res) => {
+    if(Object.keys(req.body).length == 0) {
+        res.json({
+            message: "Você precisa enviar algum valor!"
+        });
+        return
+    }
+
+    try {
+        await Person.replaceOne({ _id: req.params.id }, req.body);
+        res.status(200).json({
+            message: "Objeto modificado com sucesso!"
+        })
+    } catch(error) {
+        res.status(404).json({
+            message: "Ocorreu um erro na sua requisição!"
+        })
+    }
+})
+
+//DELETE
+routes.delete('/delete', async (req, res) => {
+    if(Object.keys(req.body).length == 0) {
+        res.json({
+            message: "Você precisa enviar algum valor!"
+        });
+        return
+    }
+    
+    try {
+        await Person.deleteOne(req.body);
+        res.status(200).json({
+            message: "Objeto correspondente foi deletado com sucesso!"
+        })
+    } catch(error) {
+        res.status(404).json({
+            message: "Ocorreu um erro na sua requisição!"
+        })
+    }
 })
 
 module.exports = routes
