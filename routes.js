2ffaf49a02be392f9f82c41907972963d40c5263
Rian <rianbarroso596@gmail.com>
2023-01-23 13:00:37 -0300

    added crud controllers

--- routes.js
+++ routes.js
@@ -1,132 +1,25 @@
 const express = require('express');
 const routes = express.Router();
-const Person = require('./src/Models/Person');
 const authController = require('./src/Controllers/authController');
-const checkToken = require('./src/Middlewares/checkTokenMiddleware');
+const crudController = require('./src/Controllers/crudController');
 
 
-//INSERT
-routes.post('/', checkToken, async (req, res) => {
-    const { nome, sobrenome, ocupacao, idade } = req.body;
-    if(!nome || !sobrenome) return res.status(422).json( { error: "os campos nome e sobrenome são obrigatórios" });
+//create route
+routes.post('/', crudController.crudCreate);
 
-    const person = { nome, sobrenome, ocupacao, idade };
+//read all route
+routes.get('/', crudController.crudRead);
 
-    try {
-        await Person.create(person);
-        const response = {
-            message: "Usuário criado com sucesso",
-            usuario_criado: req.body,
-            request: {
-                type: "GET",
-                descricao: "ver todos os usuários",
-                url: "http://localhost:3000/"
-            }
-        }
-        return res.status(200).json(response);
-    } catch(err) {
-        return res.status(500).json({ message: err })
-    }
-})
+//read one route
+routes.get('/:id', crudController.crudFindOne);
 
+//update route
+routes.patch('/update/:id', crudController.crudUpdate);
 
-//SELECT
-routes.get('/', (req, res) => {
-    Person.find({}, (err, docs) => {
-        if (err) return res.status(500).json({ message: "ocorreu um erro em sua requisição" });
-        
-        const response = {
-            quantidade: docs.length,
-            users: docs.map(user => {
-                return {
-                    _id: user._id,
-                    nome: user.nome,
-                    sobrenome: user.sobrenome,
-                    request: {
-                        type: "GET",
-                        descricao: "retorna todas as informações",
-                        url: `http://localhost:3000/${user.id}`
-                    }
-                }
-            })
-        }
-        return res.status(200).json(response)
-    })
-})
-
-
-//FIND ONE
-routes.get('/:id', checkToken, async (req, res) => {
-   const docFind = await Person.findOne({ _id: req.params.id }, (err, doc) => {
-    if (err) return res.status(500).json({ message: err});
-    if (Object.keys(doc).length == 0) return req.status(404).json({ message: "usuário não existe" });
-
-    const response = {
-        _id: doc._id,
-        nome: doc.nome,
-        sobrenome: doc.sobrenome,
-        ocupacao: doc.ocupacao,
-        idade: doc.idade,
-        request: {
-            type: "GET",
-            descricao: "ver todos os usuários",
-            url: "http://localhost:3000/"
-        }
-    }
-
-    return res.status(200).json(response);
-   })
-})
-
-
-//UPDADE
-routes.patch('/update/:id', checkToken, async (req, res) => {
-    if(Object.keys(req.body).length == 0) return res.json({ message: "Você precisa enviar algum valor!" });
-    const paramId = { _id: req.params.id };
-    const bodyUpdate = req.body
-
-    try {
-        const userUpdate = await Person.updateOne(paramId, bodyUpdate);
-        const response = {
-            message: "usuário atualizado com sucesso!",
-            request: {
-                type: "GET",
-                descricao: "visualizar usuário",
-                url: `http://localhost:3000/${req.params.id}`
-            }
-        }
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ message: err });
-    }
-})
-
-
-//DELETE
-routes.delete('/delete', checkToken, async (req, res) => {
-    if(Object.keys(req.body).length == 0) return res.json({ message: "Você precisa enviar algum valor!" });
-    
-    const deleteUser = await Person.deleteOne(req.body);
-    const response = {
-        message: "usuário(s) removido(s) com sucesso",
-        quantidade_removidos: deleteUser.deletedCount,
-        request: {
-            type: "INSERT",
-            descricao: "Inserir um novo usuário",
-            url: "http://localhost:3000/",
-            metodo: {
-                nome: "String",
-                sobrenome: "String",
-                ocupacao: "String",
-                idade: "Number"
-            }
-        }
-    }
-    res.status(200).json(response)
-})
+//delete route
+routes.delete('/delete', crudController.crudDelete);
 
 //Authenticatoin Routes
-
 routes.post('/auth/register', authController.apiRegister);
 routes.post('/auth/login', authController.apiLogin);
 
---
+++ src/Controllers/crudController.js
@@ -0,0 +1,113 @@
+const Person = require('../Models/Person')
+const checkToken = require('../Middlewares/checkTokenMiddleware');
+
+exports.crudCreate = checkToken, async (req, res) => {
+    const { nome, sobrenome, ocupacao, idade } = req.body;
+    if(!nome || !sobrenome) return res.status(422).json( { error: "os campos nome e sobrenome são obrigatórios" });
+
+    const person = { nome, sobrenome, ocupacao, idade };
+
+    try {
+        await Person.create(person);
+        const response = {
+            message: "Usuário criado com sucesso",
+            usuario_criado: req.body,
+            request: {
+                type: "GET",
+                descricao: "ver todos os usuários",
+                url: "http://localhost:3000/"
+            }
+        }
+        return res.status(200).json(response);
+    } catch(err) {
+        return res.status(500).json({ message: err })
+    }
+};
+
+exports.crudRead = (req, res) => {
+    Person.find({}, (err, docs) => {
+        if (err) return res.status(500).json({ message: "ocorreu um erro em sua requisição" });
+        
+        const response = {
+            quantidade: docs.length,
+            users: docs.map(user => {
+                return {
+                    _id: user._id,
+                    nome: user.nome,
+                    sobrenome: user.sobrenome,
+                    request: {
+                        type: "GET",
+                        descricao: "retorna todas as informações",
+                        url: `http://localhost:3000/${user.id}`
+                    }
+                }
+            })
+        }
+        return res.status(200).json(response)
+    })
+};
+
+exports.crudFindOne = checkToken, (req, res) => {
+     Person.findOne({ _id: req.params.id }, (err, doc) => {
+     if (err) return res.status(500).json({ message: err});
+     if (Object.keys(doc).length == 0) return req.status(404).json({ message: "usuário não existe" });
+ 
+     const response = {
+         _id: doc._id,
+         nome: doc.nome,
+         sobrenome: doc.sobrenome,
+         ocupacao: doc.ocupacao,
+         idade: doc.idade,
+         request: {
+             type: "GET",
+             descricao: "ver todos os usuários",
+             url: "http://localhost:3000/"
+         }
+     }
+ 
+     return res.status(200).json(response);
+    })
+ };
+
+ exports.crudUpdate = checkToken, async (req, res) => {
+    if(Object.keys(req.body).length == 0) return res.json({ message: "Você precisa enviar algum valor!" });
+    const paramId = { _id: req.params.id };
+    const bodyUpdate = req.body
+
+    try {
+        const userUpdate = await Person.updateOne(paramId, bodyUpdate);
+        const response = {
+            message: "usuário atualizado com sucesso!",
+            request: {
+                type: "GET",
+                descricao: "visualizar usuário",
+                url: `http://localhost:3000/${req.params.id}`
+            }
+        }
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+}
+
+exports.crudDelete = checkToken, async (req, res) => {
+    if(Object.keys(req.body).length == 0) return res.json({ message: "Você precisa enviar algum valor!" });
+    
+    const deleteUser = await Person.deleteOne(req.body);
+    const response = {
+        message: "usuário(s) removido(s) com sucesso",
+        quantidade_removidos: deleteUser.deletedCount,
+        request: {
+            type: "INSERT",
+            descricao: "Inserir um novo usuário",
+            url: "http://localhost:3000/",
+            metodo: {
+                nome: "String",
+                sobrenome: "String",
+                ocupacao: "String",
+                idade: "Number"
+            }
+        }
+    }
+    res.status(200).json(response)
+}
